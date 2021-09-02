a80d0cc1b31ea39b12bda6fbcf8c9e470f50fb72
Grigory Bordyugov <gregbg@gmail.com>
2021-09-02 18:39:27 +0200

    just fooling around

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn shuntyard(input: &str) -> String {
+    let mut stack: Vec<char> = Vec::new();
+    let mut output = String::new();
+    let numbers = "0123456789";
+    let ops = "-+*/";
+
+    for c in input.chars() {
+        if numbers.contains(c) {
+            output.push(c);
+        }
+        if ops.contains(c) {
+            stack.push(c);
+        }
+    }
+    output
+}
+
 fn main() {
+    println!("{}", shuntyard("014+145-/145"));
     println!("Hello, world!");
 }
