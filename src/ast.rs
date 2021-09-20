ade0dc4d265d09fdcea85880b8693ce100b06afe
Grigory Bordyugov <gregbg@gmail.com>
2021-09-20 13:03:06 +0200

    moving things around

--- src/ast.rs
+++ src/ast.rs
@@ -1,10 +1,6 @@
 // use std::collections::HashMap;
+use crate::tokenizer::{Op, Token};
 use std::rc::Rc;
-
-/**
- * "abcd|efgh" => Alter("abcd", "efgh")
- * "abc(d|e)fgh" -> Concat("abc", Concat(Alter("d", "e"), "fgh"))
- */
 
 #[derive(Debug)]
 pub enum AstNode {
@@ -21,43 +17,7 @@ pub enum AstNode {
 }
 
 impl AstNode {
-    /*
-        fn from_str(s: &str) -> Self {
-            let mut precedence: HashMap<char, u8> = HashMap::new();
-            precedence.insert('|', 1);
-            unimplemented!()
-        }
-    */
-}
-
-#[cfg(test)]
-mod tests {
-    use super::AstNode;
-    use super::Rc;
-
-    #[test]
-    fn empty_can_be_created() {
-        let _e = AstNode::Empty;
-    }
-
-    #[test]
-    fn char_can_be_created() {
-        let _c = AstNode::Char('a');
-    }
-
-    #[test]
-    fn concat_can_be_created() {
-        let _c = AstNode::Concat {
-            left: Rc::new(AstNode::Char('l')),
-            right: Rc::new(AstNode::Char('r')),
-        };
-    }
-
-    #[test]
-    fn alter_can_be_created() {
-        let _a = AstNode::Alter {
-            left: Rc::new(AstNode::Char('l')),
-            right: Rc::new(AstNode::Char('r')),
-        };
+    fn from_rpn_tokens(tokens: Vec<Token>) -> Self {
+        unimplemented!()
     }
 }
